5c9449fd7dfbe780ab253ab46abebcb3a5739e01
Tristan Andrus <tristan@tristanandrus.com>
2026-08-05 15:36:14 -0600

    Good progress
    Server seems to spit out contents of file like it's meant to do.

--- server/server.c
+++ server/server.c
@@ -22,8 +22,7 @@
 #endif
 #include <stdatomic.h>
 
-#define MAX_BUF_SIZE (1024 * 1024)
-
+#define MAX_BUF_SIZE (4 * 1024)
 
 void log_error(const char *msg)
 {
@@ -69,21 +68,174 @@ int bind_to_localhost(int sockfd)
     return 0;
 }
 
-int handle_connection(int sockfd)
+int write_all(int fd, const char* buf, size_t len)
+{
+    ssize_t bytes_left = len;
+    ssize_t bytes_written_this_round = 0;
+    int fail_count = 0;
+    const int MAX_FAIL_COUNT = 20;
+
+    while (bytes_left > 0)
+    {
+        bytes_written_this_round = write(fd, (void*)buf, bytes_left);
+        if (bytes_written_this_round <= 0)
+        {
+            log_error("Could not write to file");
+            fail_count++;
+            if (fail_count > MAX_FAIL_COUNT)
+            {
+                fprintf(stderr, "!! Could not write buf \"%s\" to fd %d. Quitting with %zd bytes to go. write() last returned %zd\n",
+                        buf,
+                        fd,
+                        bytes_left,
+                        bytes_written_this_round
+                        );
+                syslog(LOG_ERR, "Too many failures writing to fd %d. Quitting.", fd);
+                return -1;
+            }
+            continue;
+        }
+        else
+        {
+            syslog(LOG_DEBUG, "Wrote %zd bytes to file", bytes_written_this_round);
+            bytes_left = bytes_left - bytes_written_this_round;
+            buf += bytes_written_this_round;
+        }
+    }
+    return 0;
+    syslog(LOG_DEBUG, "Finished writing all bytes to fd %d", fd);
+}
+
+int duplicate_data_across_fds(int input_fd, int output_fd)
+{
+    ssize_t num_bytes_read = 0;
+    char buffer[MAX_BUF_SIZE];
+
+    while(( num_bytes_read = read(input_fd, buffer, MAX_BUF_SIZE)) > 0)
+    {
+        if (-1 == write_all(output_fd, buffer, num_bytes_read))
+        {
+            log_error("Could not write all bytes to the buffer.");
+            return -1;
+        }
+    }
+
+    return 0;
+}
+
+void print_all(const char* msg)
+{
+    size_t len = strlen(msg);
+
+    size_t num_newlines = 0;
+    for (size_t i = 0; i < len; ++i)
+    {
+        if (msg[i] == '\n')
+        {
+            num_newlines++;
+        }
+    }
+
+    size_t new_len = len + num_newlines;
+
+    char* result = calloc(len, sizeof(char));
+
+
+    for(size_t i = 0, j = 0; i < len; ++i, ++j)
+    {
+        if(msg[i] == '\n')
+        {
+            result[j++] = '\\';
+            result[j] = 'n';
+        }
+        else {
+            result[j] = msg[i];
+        }
+    }
+
+    printf("->>%s<<-", result);
+    free(result);
+}
+
+void spit_file_back_out_to_socket(int sockfd, int diskfd)
+{
+    // Rewind to beginning of file
+    off_t seek_res = lseek(diskfd, 0, SEEK_SET);
+    if (0 != seek_res)
+    {
+        log_error("Could not read from beginning of file");
+        exit(13);
+    }
+
+    duplicate_data_across_fds(diskfd, sockfd);
+
+#if 0
+    ssize_t bytes_read = 0;
+    while (get_run_flag() == 1) {
+        const size_t buf_size = 2048;
+        char *buf = calloc(buf_size, sizeof(char));
+        ssize_t this_round_bytes_read = read(diskfd, (void*)buf, buf_size);
+        if (this_round_bytes_read <= -1)
+        {
+            log_error("Could not read back from file");
+            exit(14);
+        }
+        else if (0 == this_round_bytes_read)
+        {
+            printf("Finished reading from file.\n");
+
+            free(buf);
+            break;
+        }
+
+        free(buf);
+    }
+#endif
+
+    if (-1 == lseek(diskfd, 0, SEEK_END))
+    {
+        log_error("Could not reset file position to end");
+        exit(15);
+    }
+}
+
+#if 0
+// Each time this is called, it will return the first packet in its internally-managed
+// buffer. It returns NULL when there is no packet available. (Read: there is no newline
+// character)
+char* extract_packets(int sockfd, char *new_data, size_t *new_data_len)
+{
+    static size_t buf_size = 1024;
+    static size_t buf_index = 0;
+    const size_t size_increment = 1024;
+    static char* longterm_buf = calloc(buf_size, sizeof(char));
+
+    // First, check if there is a newline in the new_data.
+
+    
+    // Size up to accomodate large messages
+    while (buf_size <  new_data_len)
+    {
+        longterm_buf = realloc(longterm_buf, buf_size + size_increment);
+        buf_size += size_increment;
+    }
+
+}
+#endif
+
+void handle_packet(int sockfd, int diskfd, char* msg, size_t len)
+{
+    printf("-> Handling packet of size %zu: \"%s\"\n", len, msg);
+    write_all(diskfd, msg, len);
+    spit_file_back_out_to_socket(sockfd, diskfd);
+}
+
+int handle_connection(int sockfd, int diskfd)
 {
     ssize_t total_bytes_read = 0;
     ssize_t this_round_bytes_read = 0;
     char *buf = malloc(MAX_BUF_SIZE);
-
-    // Open file to write to.
-    int file_mode = S_IWGRP | S_IWUSR | S_IRGRP | S_IRUSR;
-    int diskfd = open("./aesdsocketdata", O_CREAT | O_APPEND | O_RDWR, file_mode);
-
-    if (NULL == buf)
-    {
-        log_error("Could not allocate more memory for buffer");
-        //exit(5);
-    }
+    char *leftovers_buf = malloc(MAX_BUF_SIZE);
 
     while(-1 != (this_round_bytes_read = recv(sockfd, (void*)buf, MAX_BUF_SIZE, 0)))
     {
@@ -92,17 +244,42 @@ int handle_connection(int sockfd)
             printf("-> Socket closed.\n");
             break;
         }
+
         printf("-> Data recvd:\n-----------------------\n"
                 "%s\n"
                 "-----------------------\n",
             buf);
-        write(diskfd, buf, this_round_bytes_read);
+
+        size_t bytes_processed = 0;
+        char* save_ptr = NULL;
+
+        char* token = NULL;
+        char* search_str = buf;
+        for(char* token = NULL; ; search_str = NULL)
+        {
+            token = strtok_r(search_str, "\n", &save_ptr);
+            if (NULL == token)
+                break;
+            else
+            {
+
+                printf("=> Token found");
+                printf("==> Packet:[");
+                print_all(token);
+                printf("]\n");
+                size_t token_len = strlen(token);
+                bytes_processed += token_len;
+                handle_packet(sockfd, diskfd, token, token_len);
+
+            }
+        }
+
         free(buf);
         buf = malloc(MAX_BUF_SIZE);
     }
     free(buf);
+    free(leftovers_buf);
     fsync(diskfd);
-    close(diskfd);
     return 0;
 }
 
@@ -117,6 +294,15 @@ int main(int argc, char** argv)
         exit(EXIT_FAILURE);
     }
 
+    // Open disk file
+    printf("-> Opening disk file.\n");
+    int file_mode = S_IWGRP | S_IWUSR | S_IRGRP | S_IRUSR;
+    int diskfd = open("/var/tmp/aesdsocketdata", O_CREAT | O_APPEND | O_RDWR, file_mode);
+    if (diskfd < 0)
+    {
+        log_error("Could not open file for writing");
+        exit(EXIT_FAILURE);
+    }
 
     // Open socket
     printf("-> Opening socket\n");
@@ -158,7 +344,7 @@ int main(int argc, char** argv)
         // at least the way I read the assignment requirements. So we will just
         // handle one at a time. The unit test will likely not do more than SOMAXCONN
         // connections simultaneously.
-        int handle_result = handle_connection(connection_socket_fd);
+        int handle_result = handle_connection(connection_socket_fd, diskfd);
         if (0 != handle_result)
         {
             fprintf(stderr, "Error handling connection from %s\n", client_address.sa_data);
@@ -168,6 +354,7 @@ int main(int argc, char** argv)
 
     printf("-> Closing socket.\n");
     close(sockfd);
+    close(diskfd);
     printf("-> Exiting.\n");
 }
 
