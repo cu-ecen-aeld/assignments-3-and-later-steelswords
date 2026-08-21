03b13b8367a69089deec658e216f681ca9b47f70
Tristan Andrus <tristan@tristanandrus.com>
2026-08-20 18:55:27 -0600

    WIP Circular buffer

--- aesd-char-driver/aesd-circular-buffer.c
+++ aesd-char-driver/aesd-circular-buffer.c
@@ -8,6 +8,7 @@
  *
  */
 
+#include <stdio.h>
 #ifdef __KERNEL__
 #include <linux/string.h>
 #else
@@ -29,10 +30,81 @@
 struct aesd_buffer_entry *aesd_circular_buffer_find_entry_offset_for_fpos(struct aesd_circular_buffer *buffer,
             size_t char_offset, size_t *entry_offset_byte_rtn )
 {
-    /**
-    * TODO: implement per description
-    */
-    return NULL;
+    struct aesd_buffer_entry *result = NULL;
+
+    // Loop through entries.
+    // To do this, start at the entry @ out_offs.
+    size_t accumulated_size = 0;
+    // For each entry,
+    for (uint8_t i = buffer->out_offs;
+            (i < buffer->in_offs && !buffer->full) || (buffer->full && i == buffer->in_offs) ;
+            i = (i + 1) % AESDCHAR_MAX_WRITE_OPERATIONS_SUPPORTED)
+    {
+        struct aesd_buffer_entry *entry = &buffer->entry[i];
+        // If the entry's length + current offset >= char_offset,
+        if ((char_offset + 1 >= accumulated_size + entry->size) && char_offset >= accumulated_size)
+        {
+            // add entry's length to current_offset
+            accumulated_size += entry->size;
+        }
+        else
+        {
+            // Set return offset to char_offset - current offset
+            *entry_offset_byte_rtn = (char_offset ) - accumulated_size;
+
+            // Return this one.
+            return entry;
+        }
+    }
+
+    return result;
+}
+
+static void print_circular_buffer(struct aesd_circular_buffer *buffer)
+{
+    const char *const cell_repr        = " [ %3zu ] ";
+    const char *const both_tag         = "    B    ";
+    const char *const in_tag           = "    I    ";
+    const char *const out_tag          = "    O    ";
+    const char *const arrow_stem       = "        ";
+    const char *const cell_repr_spaces = "         ";
+    printf(
+"BUFFER: [full: %s.  in_offs=%u    out_offs=%u ]\n", buffer->full? "Y" : "N",
+(unsigned int)buffer->in_offs, (unsigned int)buffer->out_offs);
+    // Print I and O for input/output offsets.
+    for (size_t i = 0; i < AESDCHAR_MAX_WRITE_OPERATIONS_SUPPORTED; ++i)
+    {
+        if (i == buffer->in_offs && i == buffer->out_offs)
+        {
+            printf("%s", both_tag);
+        }
+        else if (i == buffer->in_offs)
+        {
+            printf("%s", in_tag);
+        }
+        else if (i == buffer->out_offs)
+        {
+            printf("%s", out_tag);
+        }
+        else
+        {
+            printf("%s", cell_repr_spaces);
+        }
+    }
+    printf("\n");
+    for (size_t i = 0; i < AESDCHAR_MAX_WRITE_OPERATIONS_SUPPORTED; ++i)
+    {
+        if (i == buffer->in_offs || i == buffer->out_offs)
+            printf("%s", arrow_stem); 
+        else
+            printf("%s", cell_repr_spaces);
+    }
+    printf("\n");
+    for (size_t i = 0; i < AESDCHAR_MAX_WRITE_OPERATIONS_SUPPORTED; ++i)
+    {
+        printf(cell_repr, buffer->entry[i].size);
+    }
+    printf("\n");
 }
 
 /**
@@ -44,9 +116,70 @@ struct aesd_buffer_entry *aesd_circular_buffer_find_entry_offset_for_fpos(struct
 */
 void aesd_circular_buffer_add_entry(struct aesd_circular_buffer *buffer, const struct aesd_buffer_entry *add_entry)
 {
-    /**
-    * TODO: implement per description
-    */
+    // Validate parameters
+    if (NULL == buffer || NULL == add_entry)
+    {
+        fprintf(stderr, "%s:%d: Error: Cannot accept a NULL parameter.",
+                __func__, __LINE__);
+        return;
+    }
+    if (add_entry->buffptr == NULL)
+    {
+        fprintf(stderr, "%s:%d: Error: Entry to be added has NULL buffer.\n",
+                __func__, __LINE__);
+        return;
+    }
+    if (buffer->out_offs > AESDCHAR_MAX_WRITE_OPERATIONS_SUPPORTED)
+    {
+        fprintf(stderr, "%s:%d: Error: Malformed aesd_circular_buffer. out_offs = %u, but max = %d",
+                __func__, __LINE__,
+                (unsigned int)buffer->out_offs,
+                AESDCHAR_MAX_WRITE_OPERATIONS_SUPPORTED
+               );
+        return;
+    }
+    if (buffer->in_offs > AESDCHAR_MAX_WRITE_OPERATIONS_SUPPORTED)
+    {
+        fprintf(stderr, "%s:%d: Error: Malformed aesd_circular_buffer. in_offs = %u, but max = %d",
+                __func__, __LINE__,
+                (unsigned int)buffer->in_offs,
+                AESDCHAR_MAX_WRITE_OPERATIONS_SUPPORTED
+               );
+        return;
+    }
+    
+    printf("===================================================================\n");
+    printf(" Adding entry with size=%zu, buffer=[%s]\n\n", add_entry->size, add_entry->buffptr);
+    printf("Circular Buffer Before adding entry:\n");
+    print_circular_buffer(buffer);
+
+    // Always write at the input offset
+    // If the buffer is full, advance the out_offs, since you want to read the oldest
+    // entry, not the newest one (the one that was just added).
+    buffer->entry[buffer->in_offs] = *add_entry;
+    if (buffer->in_offs == buffer->out_offs)
+    {
+        if (buffer->full)
+        {
+            buffer->out_offs++;
+            buffer->out_offs %= AESDCHAR_MAX_WRITE_OPERATIONS_SUPPORTED;
+        }
+    }
+    buffer->in_offs++;
+    buffer->in_offs %= AESDCHAR_MAX_WRITE_OPERATIONS_SUPPORTED;
+
+    if (buffer->in_offs == buffer->out_offs)
+    {
+        buffer->full = true;
+    }
+    else
+    {
+        buffer->full = false;
+    }
+
+    printf("Circular Buffer After adding entry:\n");
+    print_circular_buffer(buffer);
+    printf("===================================================================\n");
 }
 
 /**
