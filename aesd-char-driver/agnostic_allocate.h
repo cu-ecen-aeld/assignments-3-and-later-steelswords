e3645f0ecba6b8c23cb1f1771a5e7e8380b8eb0d
Tristan Andrus <tristan@tristanandrus.com>
2026-09-15 09:21:32 -0600

    Fix includes in agnostic_allocate

--- aesd-char-driver/agnostic_allocate.h
+++ aesd-char-driver/agnostic_allocate.h
@@ -2,9 +2,11 @@
 #define AGNOSTIC_ALLOCATE_H_
 
 #ifdef __KERNEL__
+#include <linux/slab.h>
 #define agnostic_zallocate(sz) kzalloc(sz, GFP_KERNEL)
 #define agnostic_free(ptr) kfree(ptr)
 #else
+#include <stdlib.h>
 #define agnostic_zallocate(sz), calloc(sz, 1)
 #define agnostic_free(ptr) free(ptr)
 #endif
